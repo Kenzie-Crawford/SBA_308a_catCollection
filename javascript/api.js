9eac6263378e58fc5ea1a49968e2e8220c7ea238
Kenzie-Crawford <kenzie.scrawford13@gmail.com>
2026-02-13 10:50:43 -0500

    restructure and api.js

---
+++ javascript/api.js
@@ -0,0 +1,42 @@
+const API_KEY = "live_Hu8ooDQc3aZDhA3Vk82FgqUMyesUjRtNUHO3m0b3qOWevvbpXKoRI2qeGrYxpKWE";
+const BASE_URL = "https://api.thecatapi.com/v1";
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "x-api-key": API_KEY
+  }
+});
+
+export async function getBreeds() {
+  const response = await api.get("/breeds");
+  return response.data;
+}
+
+export async function searchCats(breedId) {
+  const response = await api.get("/images/search", {
+    params: {
+      limit: 8,
+      breed_ids: breedId
+    }
+  });
+
+  return response.data;
+}
+
+export async function addFavorite(imageId) {
+  const response = await api.post("/favourites", {
+    image_id: imageId
+  });
+
+  return response.data;
+}
+
+export async function getFavorites() {
+  const response = await api.get("/favourites");
+  return response.data;
+}
+
+export async function deleteFavorite(favId) {
+  await api.delete(`/favourites/${favId}`);
+}
